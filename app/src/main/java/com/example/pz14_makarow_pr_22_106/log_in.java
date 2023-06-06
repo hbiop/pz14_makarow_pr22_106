3630606a8d017125c23f1c3c9ad48503e2109390
starterov <starterror@duck.com>
2023-06-06 11:45:27 +0700

    start

---
+++ app/src/main/java/com/example/pz14_makarow_pr_22_106/log_in.java
@@ -0,0 +1,14 @@
+package com.example.pz14_makarow_pr_22_106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class log_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
---
+++ app/src/main/java/com/example/pz14_makarow_pr_22_106/tip.java
@@ -0,0 +1,14 @@
+package com.example.pz14_makarow_pr_22_106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip);
+    }
+}
